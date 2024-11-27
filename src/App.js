d0d0460bf8ebabeca00930be4a389d36ec63bfa0
OyedunOye <oyesinaoyedun@yahoo.com>
2024-11-27 01:02:36 +0100

    trafalgar app outline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+import { Navbar, Introduction, AboutUs, MobileApp, Services,Publications, Testimonials, Footer } from './components';
+
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Introduction />
+      <Services />
+      <AboutUs />
+      <MobileApp />
+      <Testimonials />
+      <Publications />
+      <Footer />
     </div>
   );
-}
 
-export default App;
+  export default App;
---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar/Navbar";
+import Introduction from "./Introduction/Introduction";
+import Services from "./Services/Services";
+import AboutUs from "./AboutUs/AboutUs";
+import MobileApp from "./MobileApp/MobileApp";
+import Testimonials from "./Testimonials/Testimonials";
+import Publications from "./Publications/Publications";
+import Footer from "./Footer/Footer";
+
+export {
+    Navbar,
+    Introduction,
+    Services,
+    AboutUs,
+    MobileApp,
+    Testimonials,
+    Publications,
+    Footer
+};
